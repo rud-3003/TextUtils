c52c254f8b434303edd8e408d8babb1bf7ea866b
Rudra Gupta <114010809+rud-3003@users.noreply.github.com>
2024-06-04 16:56:40 +0530

    Adjusting spaces

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert(props) {
     return (
-        <div style={{height: '50px'}}>
+        <div style={{height: '40px'}}>
             {props.alert && <div className={`alert alert-${props.alert.type.toLowerCase()} alert-dismissible fade show`} role="alert">
                 <strong>{props.alert.type}</strong>: {props.alert.msg}
             </div>}
